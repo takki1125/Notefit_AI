16580d607b361bfafb33c3dac09099647ce277e3
takki1125 <t.ryuya112511@gmail.com>
2026-03-08 15:09:10 +0900

    Create time.ts

---
+++ utils/time.ts
@@ -0,0 +1,9 @@
+export function formatTime(totalSeconds: number): string {
+  const h = Math.floor(totalSeconds / 3600);
+  const m = Math.floor((totalSeconds % 3600) / 60)
+    .toString()
+    .padStart(2, '0');
+  const s = (totalSeconds % 60).toString().padStart(2, '0');
+  return h > 0 ? `${h}:${m}:${s}` : `${m}:${s}`;
+}
+
