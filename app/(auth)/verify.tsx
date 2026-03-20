c311b1215701b7f2b72f09a88a78d89a1d6b547d
takki1125 <t.ryuya112511@gmail.com>
2026-03-20 21:49:48 +0900

    Update verify.tsx

--- app/(auth)/verify.tsx
+++ app/(auth)/verify.tsx
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 import {
   ActivityIndicator,
   Alert,
-  SafeAreaView,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
 } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { Mail } from 'lucide-react-native';
 import { sendEmailVerification, signOut } from 'firebase/auth';
 
